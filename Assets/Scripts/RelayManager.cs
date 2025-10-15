1b622870e282e4f34aa20d0e0a784e5c9567457d
Gloria Zhu <gloriazhu00@gmail.com>
2025-10-15 11:26:24 -0400

    fix merge conflicts

--- Assets/Scripts/RelayManager.cs
+++ Assets/Scripts/RelayManager.cs
@@ -8,15 +8,27 @@ using TMPro;
 using Unity.Netcode;
 using Unity.Networking.Transport.Relay;
 using Unity.Netcode.Transports.UTP;
+using System.Threading.Tasks;
+using UnityEngine.SceneManagement;
 
 
 public class RelayManager : MonoBehaviour
 {
-    [SerializeField] Button hostButton;
-    [SerializeField] Button joinButton;
-    [SerializeField] TMP_InputField joinInput;
-    [SerializeField] TMP_Text codeText;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static RelayManager Instance;
+    string player1Username;
+    string player2Username;
+
+    // These fields are still fine.
+    public GameObject Player;
+    public GameObject Architect;
+
+    public string[] levels = {"DaftPunkLevel, Snoc"};
+
+    private void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
 
     private UnityTransport transport;
     // This string will be "wss" for WebGL builds and "dtls" for all other platforms
@@ -27,51 +39,152 @@ public class RelayManager : MonoBehaviour
 
         // This is a preprocessor directive that will only compile the code inside
         // if the build target is WebGL
-        #if UNITY_WEBGL
+#if UNITY_WEBGL
             connectionType = "wss";
         // Also enable WebSockets in the transport for WebGL
         transport.UseWebSockets = true;
-        #else
-            transport.UseWebSockets = false;
-        #endif
-        
-        await UnityServices.InitializeAsync();
-        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+#else
+        transport.UseWebSockets = false;
+#endif
 
-        hostButton.onClick.AddListener(CreateRelay);
-        joinButton.onClick.AddListener(() => JoinRelay(joinInput.text));
+        try
+        {
+            await UnityServices.InitializeAsync();
+            Debug.Log("Unity Services initialized");
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            Debug.Log($"Signed in as player {AuthenticationService.Instance.PlayerId}");
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError($"Unity Services init failed: {e}");
+        }
+
+        Debug.Log("started relay manager");
     }
 
-    async void CreateRelay()
+    // CreateRelay starts a relay session and returns the allocation ID (gamecode)
+    public async Task<string> CreateRelay()
     {
-        Allocation allocation = await RelayService.Instance.CreateAllocationAsync(1);
+        try
+        {
+            Debug.Log("Creating Relay Allocation...");
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(1);
+            Debug.Log("Allocation created!");
 
-        string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
-        codeText.text = "Code: " + joinCode;
+            string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+            Debug.Log($"Got join code: {joinCode}");
 
-        var relayServerData = AllocationUtils.ToRelayServerData(allocation, connectionType);
+            var relayServerData = AllocationUtils.ToRelayServerData(allocation, connectionType);
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+            NetworkManager.Singleton.StartHost();
 
-        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
-
-        NetworkManager.Singleton.StartHost();
-        HideCanvas();
+            return joinCode;
+        }
+        catch (RelayServiceException e)
+        {
+            Debug.LogError($"Relay service error: {e.Message}");
+            return null;
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError($"Unexpected error creating relay: {e}");
+            return null;
+        }
     }
 
-    async void JoinRelay(string joinCode)
+    // JoinRelay attempts to join a session with the provided gamecode and returns
+    // if it is successful or not
+    // TODO: check for failures. for now, returning true all the time
+    public async Task<bool> JoinRelay(string joinCode)
     {
         JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
         var relayServerData = AllocationUtils.ToRelayServerData(joinAllocation, connectionType);
 
         NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
-
         NetworkManager.Singleton.StartClient();
-        HideCanvas();
+
+        return true;
     }
-    
-    void HideCanvas()
+
+    public void SetPlayerUsernames(string player1Username, string player2Username)
     {
-        hostButton.gameObject.SetActive(false);
-        joinButton.gameObject.SetActive(false);
-        joinInput.gameObject.SetActive(false);
+        this.player1Username = player1Username;
+        this.player2Username = player2Username;
+    }
+
+    public void SpawnPlayer()
+    {
+        const ulong clientId = NetworkManager.ServerClientId;
+        Debug.Log("spawning player; serverclient id is " + clientId);
+        GameObject prefabToSpawn = (clientId == 0) ? Architect : Player;
+        Debug.Log(prefabToSpawn);
+        GameObject playerInstance = Instantiate(prefabToSpawn);
+        // // Important: Make sure the player instance is spawned at a valid position.
+        // playerInstance.transform.position = new Vector3(0, 2, 0); // Example spawn position
+        playerInstance.GetComponent<NetworkObject>().SpawnAsPlayerObject(clientId, true);
+    }
+
+    // public void LoadLevel(int levelIndex)
+    // {
+    //     Debug.Log("attempting to load level index " + levelIndex);
+    //     if (NetworkManager.Singleton.IsHost)
+    //     {
+    //         Debug.Log("is host, loading level " + levels[levelIndex]);
+    //         NetworkManager.Singleton.SceneManager.LoadScene(levels[levelIndex], LoadSceneMode.Single);
+    //         Debug.Log("loaded level " + levels[levelIndex]);
+    //     }
+    //     // TODO: check levelIndex within bounds
+    // }
+    public void LoadLevel(int levelIndex)
+    {
+        if (levelIndex < 0 || levelIndex >= levels.Length)
+        {
+            Debug.LogError("Invalid level index");
+            return;
+        }
+
+        if (!NetworkManager.Singleton.IsHost)
+        {
+            Debug.Log("only hosts can start levels for now");
+            return;
+        }
+
+        string levelName = levels[levelIndex];
+        Debug.Log("Loading networked level: " + levelName);
+
+        // Subscribe to the OnLoadComplete before starting scene load
+        NetworkManager.Singleton.SceneManager.OnLoadComplete += OnSceneLoaded;
+
+        if (NetworkManager.Singleton.IsHost)
+        {
+            NetworkManager.Singleton.SceneManager.LoadScene(levelName, LoadSceneMode.Single);
+        }
+    }
+
+    private void OnSceneLoaded(ulong clientId, string sceneName, LoadSceneMode mode)
+    {
+        Debug.Log($"Scene '{sceneName}' loaded for client {clientId}");
+
+        if (!NetworkManager.Singleton.IsServer)
+            return;
+
+        // Spawn Architect for host, Player for client(s)
+        foreach (var client in NetworkManager.Singleton.ConnectedClientsList)
+        {
+            if (client.PlayerObject != null)
+                continue;
+
+            GameObject prefab = client.ClientId == NetworkManager.Singleton.LocalClientId
+                ? Architect
+                : Player;
+
+            GameObject instance = Instantiate(prefab);
+            instance.GetComponent<NetworkObject>().SpawnAsPlayerObject(client.ClientId);
+
+            Debug.Log($"Spawned {prefab.name} for client {client.ClientId}");
+        }
+
+        // Only need this once per load
+        NetworkManager.Singleton.SceneManager.OnLoadComplete -= OnSceneLoaded;
     }
 }
--- Assets/Scripts/StartUI/StartUIManager.cs
+++ Assets/Scripts/StartUI/StartUIManager.cs
@@ -4,6 +4,8 @@ using UnityEngine.UI;
 
 public class StartUIManager : MonoBehaviour
 {
+    private RelayManager relayManager;
+
     [Header("UI Screens")]
     public GameObject splashScreen;
     public GameObject hostScreen;
@@ -17,6 +19,7 @@ public class StartUIManager : MonoBehaviour
 
     [Header("Host Game Screen Inputs")]
     public Button player1StartGameButton;
+    public TMP_Text  gamecodeTemp;
     public TMP_InputField player1UsernameInputField;
 
     [Header("Join Game Screen Inputs")]
@@ -26,12 +29,16 @@ public class StartUIManager : MonoBehaviour
     public Button player2JoinButton;
     public TMP_InputField gamecodeInputField;
 
+    [Header("Level Select")]
+    public GameObject levelSelectParent;
+
     public void Start()
     {
         baseScreen.SetActive(true);
         splashScreen.SetActive(true);
         hostScreen.SetActive(false);
         joinScreen.SetActive(false);
+        levelSelectParent.SetActive(false);
 
         player2CenterGameInfo.SetActive(false);
     }
@@ -48,12 +55,18 @@ public class StartUIManager : MonoBehaviour
     }
 
     // Splash screen routing
-    public void OnHostButtonPress()
+    public async void OnHostButtonPress()
     {
+        // Blocks and returns the resulting game ID
+        string gamecode = await RelayManager.Instance.CreateRelay();
+        gamecodeTemp.text = gamecode;
+
         splashScreen.SetActive(false);
         hostScreen.SetActive(true);
         joinScreen.SetActive(false);
-        Debug.Log("host button was pressed");
+        Debug.Log("hosting new game with gamecode " + gamecode);
+
+        joinGameButton.interactable = false;
     }
     public void OnJoinButtonPressed()
     {
@@ -61,29 +74,77 @@ public class StartUIManager : MonoBehaviour
         hostScreen.SetActive(false);
         joinScreen.SetActive(true);
         Debug.Log("join button was pressed");
+
+        hostGameButton.interactable = false;
+    }
+
+    private void ShowLevels()
+    {
+        Button[] buttons = levelSelectParent.GetComponentsInChildren<Button>();
+
+        string[] levels = RelayManager.Instance.levels;
+        for (int i = 0; i < levels.Length; i++)
+        {
+            int index = i; // prevent closure issue
+            Debug.Log("hooked up level " + levels[i] + " to button idx " + i);
+            buttons[i].GetComponentInChildren<TMP_Text>().text = levels[i];
+            buttons[i].onClick.RemoveAllListeners();
+            buttons[i].onClick.AddListener(() =>
+            {
+                RelayManager.Instance.LoadLevel(index);
+            });
+        }
+        // set other buttons to non-interactable. proper way to do this later is to
+        // just spawn in the required # of buttons
+        for (int i = levels.Length; i < buttons.Length; i++)
+        {
+            buttons[i].interactable = false;
+            buttons[i].GetComponentInChildren<TMP_Text>().text = " ";
+        }
+
+        levelSelectParent.SetActive(true);
     }
 
     // Host game screen routing
-    public void OnPlayer1StartButtonPress()
+    private void OnPlayer1StartButtonPress()
     {
-        // TODO: Check for player 2 info here
-        Debug.Log("start game button pressed by player 1! TODO: attempt to start");
-        Debug.Log("player 1 username: " + player1UsernameInputField.text);
+        Debug.Log("start game button pressed by player 1! TODO: check that a player 2 has actually joined, then retrieve the usernames");
+        // Debug.Log("player 1 username: " + player1UsernameInputField.text);
+
+        RelayManager.Instance.SetPlayerUsernames("player1Username", "player2Username");
+        // Start game as host
+        ShowLevels();
     }
 
     // Join game screen routing
-    public void OnPlayer2StartButtonPress()
+    private void OnPlayer2StartButtonPress()
     {
-        // TODO: Check for player 1 info here
-        Debug.Log("start game button pressed by player 2! TODO: attempt to start");
-        Debug.Log("player 2 username: " + player2UsernameInputField.text);
+        Debug.Log("start game button pressed by player 2! TODO: retrieve the players' usernames properly");
+        // Debug.Log("player 2 username: " + player2UsernameInputField.text);
+
+        RelayManager.Instance.SetPlayerUsernames("player1Username", "player2Username");
+        // Start game as player
+        ShowLevels();
     }
-    public void OnPlayer2JoinPressed()
+
+    public async void OnPlayer2JoinPressed()
     {
+        // TODO later: disable UI while this function is blocking
         string gamecode = gamecodeInputField.text;
-        // TODO: Search for game with specified gamecode here and attempt to join
-        Debug.Log("join with gamecode pressed by player 2 with input " + gamecode + ". TODO: look for lobby");
-        Debug.Log("assuming game found successfully, show center panel:");
-        player2CenterGameInfo.SetActive(true);
+
+        // Blocks and returns the resulting success/failure
+        bool joinSuccess = await RelayManager.Instance.JoinRelay(gamecode);
+
+        if (joinSuccess)
+        {
+            Debug.Log("successfully joined game with ID " + gamecode);
+            player2CenterGameInfo.SetActive(true);
+            player2StartGameButton.interactable = false; // TODO: reevaluate this. is host the only one who can hit start?
+        }
+        else
+        {
+            Debug.Log("game with code " + gamecode + " not found");
+            // TODO handle this case later
+        }
     }
 }
